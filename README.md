4b5c1c86e4b7be7645c2e9ce7f7ec598e92c531e
Unai Aguilera <unai.aguilera@deusto.es>
2015-09-23 14:36:25 +0200

    Test information updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # agglo-simulator
+
+[![Build Status](https://magnum.travis-ci.com/unaguil/agglo-simulator.svg?token=fJZNuvpQu2CHYrmKy2jB&branch=master)](https://magnum.travis-ci.com/unaguil/agglo-simulator)
--- tests.py
+++ tests.py
@@ -4,21 +4,25 @@ from pymobility.models.mobility import RandomWaypoint
 
 class TestDeviceGenerator(unittest.TestCase):
 
-  def test_generation(self):
-     model = RandomWaypoint(200, dimensions=(100, 100), velocity=(0.1, 1.0), wt_max=1.0)
+    def test_generation(self):
+        model = RandomWaypoint(200, dimensions=(100, 100), velocity=(0.1, 1.0), wt_max=1.0)
 
-     devices_gen = devices_generator(model, accuracy=(20.0, 30.0))
+        devices_gen = devices_generator(model, accuracy=(20.0, 30.0))
 
-     for i in range(10):
-         devices = next(devices_gen)
-         self.assertEqual(200, len(devices))
+        for i in range(10):
+            devices = next(devices_gen)
+            self.assertEqual(200, len(devices))
 
-         for node_id, device in devices.items():
+            device_ids = set([])
+            for node_id, device in devices.items():
+                device_ids.add(node_id)
+                self.assertTrue(0 <= device.position[0] <= 100)
+                self.assertTrue(0 <= device.position[1] <= 100)
 
-             self.assertTrue(0 <= device.position[0] <= 100)
-             self.assertTrue(0 <= device.position[1] <= 100)
+                self.assertTrue(20.0 <= device.accuracy <= 30.0)
 
-             self.assertTrue(20.0 <= device.accuracy <= 30.0)
+            self.assertEquals(200, len(devices.keys()))
+            self.assertEquals(200, len(device_ids))
 
 if __name__ == '__main__':
     unittest.main()
