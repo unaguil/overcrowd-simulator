dcd927831f152ca2f07d2336724dd7aee226bb5e
Unai Aguilera <unai.aguilera@deusto.es>
2015-09-24 17:58:00 +0200

    Implementation of tree intersection

--- grid_manager.py
+++ grid_manager.py
@@ -2,6 +2,7 @@ import shapely.geometry as geometry
 from io import StringIO
 import numpy
 import time
+import math
 
 def timeit(method):
 
@@ -36,12 +37,25 @@ class Cell(object):
     def density(self):
         return self.occupation / self.manager.cell_area
 
+class InvalidCellDivisionException(Exception):
+
+    def __init__(self, message):
+        super(InvalidCellDivisionException, self).__init__(message)
+
 class GridManager(object):
 
-    def __init__(self, dimensions, n_cells=(10, 10)):
+    def __init__(self, dimensions, n_cells=(12, 12)):
         self.dimensions = dimensions
         self.n_cells = n_cells
+
+        if not self.__is_pow(n_cells):
+            raise InvalidCellDivisionException('Error. (%d, %d) is not a power of two.' % n_cells)
+        elif not self.__is_symmetric(n_cells):
+            raise InvalidCellDivisionException('Error. (%d, %d) is not symmetric.' % n_cells)
+
         self.area = float(self.dimensions[0] * self.dimensions[1])
+
+        self.max_deep = int(math.log(self.n_cells[0], 2)) - 1
 
         self.cell_dimensions = (
             dimensions[0] / float(n_cells[0]),
@@ -51,6 +65,12 @@ class GridManager(object):
         self.cell_area = self.cell_dimensions[0] * self.cell_dimensions[1]
 
         self.__clear_cells()
+
+    def __is_pow(self, index):
+        return math.log(index[0], 2).is_integer() and math.log(index[1], 2).is_integer()
+
+    def __is_symmetric(self, index):
+        return index[0] == index[1]
 
     def __clear_cells(self):
         self.cells = []
@@ -72,30 +92,22 @@ class GridManager(object):
 
         for device in devices:
             circle = self.__create_circle(device)
-
-            common_cells = []
-            total_common = 0.0
-            for row_index in range(self.rows):
-                for column_index in range(self.columns):
-                    cell = self[row_index, column_index]
-                    common = circle.intersection(cell.box)
-
-                    if not common.is_empty:
-                        common_cells.append((cell, common.area ))
-                        total_common += common.area
+            common_cells = self.__tree_intersection(circle, self.max_deep, self.dimensions)
+            total_common = self.__total_cells_area(common_cells)
 
             missing = (circle.area - total_common) / circle.area
-            for cell, common_area in common_cells:
+            for cell_index, common_area in common_cells:
                 cell_ratio = common_area / float(total_common)
-                cell.occupation += common_area / circle.area + cell_ratio * missing
+                self[cell_index].occupation += common_area / circle.area + cell_ratio * missing
 
     def __split(self, dimensions, origin=(0, 0)):
-        cell_size = (dimensions[0] / 2.0, dimensions[1] / 2.0)
+        cell_size = dimensions[0] / 2.0, dimensions[1] / 2.0
+
         cells = (
-            (origin[0], origin[0], cell_size[0], cell_size[1]),
-            (cell_size[0], origin[0], cell_size[0] * 2, cell_size[1]),
-            (origin[0], cell_size[1], cell_size[0], cell_size[1] * 2),
-            (cell_size[1], cell_size[1], cell_size[0] * 2, cell_size[1] * 2)
+            (origin[0], origin[1], origin[0] + cell_size[0], origin[1] + cell_size[1]),
+            (origin[0] + cell_size[0], origin[1], origin[0] + cell_size[0] * 2, origin[1] + cell_size[1]),
+            (origin[0], origin[1] + cell_size[1], origin[0] + cell_size[0], origin[1] + cell_size[1] * 2),
+            (origin[0] + cell_size[1], origin[1] + cell_size[1], origin[0] + cell_size[0] * 2, origin[1] + cell_size[1] * 2)
         )
 
         return cells, cell_size
@@ -110,9 +122,9 @@ class GridManager(object):
         elif num == 3:
             return (1, 1)
 
-    def __adjust_row_column(self, current, index):
+    def __adjust_row_column(self, current, index, factor):
         adjusted = (
-            (current[0] * 2 + index[0][0], current[1] * 2 + index[0][1]),
+            (current[0] * factor + index[0][0], current[1] * factor + index[0][1]),
             index[1]
         )
 
@@ -126,20 +138,24 @@ class GridManager(object):
             box = geometry.box(*cell)
             common =  box.intersection(shape)
             if not common.is_empty:
-                matrix_index = self.__get_row_column(index)
+                current_index = self.__get_row_column(index)
                 if n < max_deep:
                     cell_origin = (cell[0], cell[1])
                     for common_cell in self.__tree_intersection(shape, max_deep, cell_size, cell_origin, n + 1):
-                        common_cell = self.__adjust_row_column(matrix_index, common_cell)
+                        factor = max_deep - n
+                        common_cell = self.__adjust_row_column(current_index, common_cell, 2**factor)
                         common_cells.append(common_cell)
                 else:
-                    common_cells.append((matrix_index, common.area))
-                    
+                    common_cells.append((current_index, common.area))
+
         return common_cells
 
-    def intersection(self, device, max_deep):
-        circle = self.__create_circle(device)
-        return self.__tree_intersection(circle, max_deep, self.dimensions)
+    def __total_cells_area(self, cells):
+        total_area = 0.0
+        for cell in cells:
+            total_area += cell[1]
+
+        return total_area
 
     def __getitem__(self, index):
         return self.cells[index[0]][index[1]]
