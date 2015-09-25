365fa11adc9826501621be290c11f860e52297ce
Unai Aguilera <unai.aguilera@deusto.es>
2015-09-25 11:16:12 +0200

    Profiling support added to simulator

--- agglo_simulator.py
+++ agglo_simulator.py
@@ -4,6 +4,13 @@ from grid_manager import GridManager
 import pygame
 from pygame.locals import *
 import numpy
+import argparse
+import cProfile
+
+def profile(devices_gen, manager, num):
+    for i in range(num):
+        devices = next(devices_gen)
+        manager.update(devices.values())
 
 ################################################################################
 ### Simulation configuration
@@ -11,14 +18,14 @@ N_DEVICES = 20
 DIMENSIONS = (100, 100)
 VELOCITY = (0.1, 1.4)
 MAX_PAUSE_TIME = 10.0  # 10 seconds
-N_CELLS = (64, 64)
+N_CELLS = (128, 128)
 DENSITY_SCALE = (0.0, 0.2)
 ################################################################################
-
 SCREEN_SIZE = (518, 518)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+################################################################################
 
 def scale_color(density, density_scale):
     scale = density_scale[1] - density_scale[0]
@@ -29,12 +36,19 @@ def scale_color(density, density_scale):
     return (red_value, 0, 0)
 
 if __name__ == '__main__':
+    description = 'Agglomeration simulator v0.1'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--profile', type=int, default=0,
+	                   	help='performs profiling instead showing density')
+
+    args = parser.parse_args()
+
     model = RandomWaypoint(nr_nodes=N_DEVICES, dimensions=DIMENSIONS,
         velocity=VELOCITY, wt_max=MAX_PAUSE_TIME)
 
     devices_gen = devices_generator(model, accuracy=(0.0, 3.0))
 
-    print("Agglomeration simulator v0.1")
+    print(description)
     print("============================")
 
     grid_manager = GridManager(dimensions=DIMENSIONS, n_cells=N_CELLS)
@@ -42,29 +56,33 @@ if __name__ == '__main__':
     print("Avg. density %.5f devices/m^2" % (N_DEVICES / grid_manager.area))
     print("Cell area: %.3f m^2" % grid_manager.cell_area)
 
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-    screen.fill(BLACK)
-    pygame.display.flip()
-
-    surface = pygame.Surface(N_CELLS)
-
-    exit = False
-    while not exit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit = True
-
-        devices = next(devices_gen)
-        grid_manager.update(devices.values())
-
-        screen.fill(WHITE)
-
-        density_matrix = grid_manager.density_matrix
-        for i in range(density_matrix.shape[0]):
-            for j in range(density_matrix.shape[1]):
-                color = scale_color(density_matrix[i, j], DENSITY_SCALE)
-                surface.set_at((i, j), color)
-
-        pygame.transform.scale(surface, SCREEN_SIZE, screen)
-
+    if args.profile == 0:
+        screen = pygame.display.set_mode(SCREEN_SIZE)
+        screen.fill(BLACK)
         pygame.display.flip()
+
+        surface = pygame.Surface(N_CELLS)
+
+        exit = False
+        while not exit:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit = True
+
+            devices = next(devices_gen)
+            grid_manager.update(devices.values())
+
+            screen.fill(WHITE)
+
+            density_matrix = grid_manager.density_matrix
+            for i in range(density_matrix.shape[0]):
+                for j in range(density_matrix.shape[1]):
+                    color = scale_color(density_matrix[i, j], DENSITY_SCALE)
+                    surface.set_at((i, j), color)
+
+            pygame.transform.scale(surface, SCREEN_SIZE, screen)
+
+            pygame.display.flip()
+    else:
+        print "Running profiler with %d iterations" % args.profile
+        cProfile.run('profile(devices_gen, grid_manager, args.profile)')
