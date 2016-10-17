139dfb7e4a7982bf513688824b8cd0eca3e5c668
Unai Aguilera <unai.aguilera@deusto.es>
2016-10-17 16:43:05 +0200

    Basic configuration updated to simulate 4YFN settings

--- experiment/basic_conf.py
+++ experiment/basic_conf.py
@@ -1,10 +1,10 @@
 configuration = {
-    'devices': 20,
-    'dimensions': (500, 500),
+    'devices': 1000,
+    'dimensions': (138, 138), # ~19000 m2
     'velocity': (0.1, 1.4),
     'accuracy': (0.0, 3.0),
     'max_pause_time': 10.0,  # 10 seconds
     'cells': (6, 6),
-    'update_interval': 5.00, # seconds
-    'sim_total_time': 60 # seconds
+    'update_interval': 10.00, # seconds
+    'sim_total_time': 120 # seconds
 }
