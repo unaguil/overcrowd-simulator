7e2c5a9825a88b7e3c6be18b7bcb713572c5a929
Unai Aguilera <unai.aguilera@deusto.es>
2016-10-18 11:18:22 +0200

    Device experiments configurations added

--- experiment/devices/run.sh
+++ experiment/devices/run.sh
@@ -2,7 +2,7 @@
 rm devices.csv
 threads=8
 
-for c in conf_0;
+for c in conf_0 conf_1 conf_2 conf_3;
 do
         echo Running with $c
         spark-submit --master local[$threads] experiment/runner.py --threads $threads --output devices.csv --conf experiment.devices.$c
